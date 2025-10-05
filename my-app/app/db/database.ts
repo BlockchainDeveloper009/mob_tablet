e5e174ff7f62bb4e6852ef51e237fcd573b20e0d
Harish Govindaswamy Krishnamurthy <harishgk0406@gmail.com>
2025-10-04 19:38:24 -0500

    publish br

--- my-app/app/db/database.ts
+++ my-app/app/db/database.ts
@@ -1,11 +1,11 @@
-//import * as SQLite from 'expo-sqlite/legacy';
-import * as SQLite from 'expo-sqlite';
+// //import * as SQLite from 'expo-sqlite/legacy';
+// import * as SQLite from 'expo-sqlite';
 
-export const getDbConnectionForLoans = (): SQLite.SQLiteDatabase => {
-  return SQLite.openDatabaseSync('loans.db');
-};
+// export const getDbConnectionForLoans = (): SQLite.SQLiteDatabase => {
+//   return SQLite.openDatabaseSync('loans.db');
+// };
 
 
-export const getDbConnectionForNutriWise = (): SQLite.SQLiteDatabase => {
-  return SQLite.openDatabaseSync('NutriWise.db');
-};
+// export const getDbConnectionForNutriWise = (): SQLite.SQLiteDatabase => {
+//   return SQLite.openDatabaseSync('NutriWise.db');
+// };
--- my-app/app/db/loanQueries.ts
+++ my-app/app/db/loanQueries.ts
@@ -1,42 +1,42 @@
-import { getDbConnectionForLoans } from './database';
-import { Loan } from '../../interfaces/Loan';
+// import { getDbConnectionForLoans } from './database';
+// import { Loan } from '../../interfaces/Loan';
 
-const db = getDbConnectionForLoans();
+// const db = getDbConnectionForLoans();
 
-// export const getAllLoans = (): Promise<Loan[]> => {
-//   return new Promise((resolve, reject) => {
-//     db.transaction(tx => {
-//       tx.executeSql(
-//         'CREATE TABLE IF NOT EXISTS loans (id INTEGER PRIMARY KEY AUTOINCREMENT, borrower TEXT, amount REAL);'
-//       );
-//       tx.executeSql(
-//         'SELECT * FROM loans;',
-//         [],
-//         (_, result:any) => {
-//           const rows = result.rows._array as Loan[];
-//           resolve(rows);
-//         },
-//         (_, error:any) => {
-//           reject(error);
-//           return false;
-//         }
-//       );
-//     });
-//   });
-// };
+// // export const getAllLoans = (): Promise<Loan[]> => {
+// //   return new Promise((resolve, reject) => {
+// //     db.transaction(tx => {
+// //       tx.executeSql(
+// //         'CREATE TABLE IF NOT EXISTS loans (id INTEGER PRIMARY KEY AUTOINCREMENT, borrower TEXT, amount REAL);'
+// //       );
+// //       tx.executeSql(
+// //         'SELECT * FROM loans;',
+// //         [],
+// //         (_, result:any) => {
+// //           const rows = result.rows._array as Loan[];
+// //           resolve(rows);
+// //         },
+// //         (_, error:any) => {
+// //           reject(error);
+// //           return false;
+// //         }
+// //       );
+// //     });
+// //   });
+// // };
 
-// export const addLoan = (loan: Omit<Loan, 'id'>): Promise<void> => {
-//   return new Promise((resolve, reject) => {
-//     db.transaction(tx => {
-//       tx.executeSql(
-//         'INSERT INTO loans (borrower, amount) VALUES (?, ?);',
-//         [loan.borrower, loan.amount],
-//         () => resolve(),
-//         (_, error:any) => {
-//           reject(error);
-//           return false;
-//         }
-//       );
-//     });
-//   });
-// };
+// // export const addLoan = (loan: Omit<Loan, 'id'>): Promise<void> => {
+// //   return new Promise((resolve, reject) => {
+// //     db.transaction(tx => {
+// //       tx.executeSql(
+// //         'INSERT INTO loans (borrower, amount) VALUES (?, ?);',
+// //         [loan.borrower, loan.amount],
+// //         () => resolve(),
+// //         (_, error:any) => {
+// //           reject(error);
+// //           return false;
+// //         }
+// //       );
+// //     });
+// //   });
+// // };
